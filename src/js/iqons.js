db7f3434df2391323bb6bbc6afb5e62f13c3b5ab
robinlinus <robinlinus@users.noreply.github.com>
2018-01-18 14:34:16 -0600

    Complete feature drafts

--- src/js/iqons.js
+++ src/js/iqons.js
@@ -4,7 +4,7 @@ class Iqons {
 
     static svg(text) {
         const hash = this._hash(text);
-        return this._svgTemplate(hash[0], hash[2], hash[3] + hash[4], hash[5] + hash[6], hash[7] + hash[8], hash[9] + hash[10], hash[11]);
+        return this._svgTemplate(hash[0], hash[2], hash[3] + hash[4], hash[5] + hash[6], hash[7] + hash[8], hash[9] + hash[10], hash[11], hash[12]);
     }
 
     static render(text, $element) {
@@ -16,14 +16,14 @@ class Iqons {
     }
 
     /* Private API */
-    static _svgTemplate(color, backgroundColor, faceNr, topNr, sidesNr, bottomNr, accentColor) {
-        return this._$svg(this._$iqons(color, backgroundColor, faceNr, topNr, sidesNr, bottomNr, accentColor));
+    static _svgTemplate(color, backgroundColor, faceNr, topNr, sidesNr, bottomNr, accentColor, gaze) {
+        return this._$svg(this._$iqons(color, backgroundColor, faceNr, topNr, sidesNr, bottomNr, accentColor), gaze);
     }
 
-    static _$iqons(color, backgroundColor, faceNr, topNr, sidesNr, bottomNr, accentColor) {
+    static _$iqons(color, backgroundColor, faceNr, topNr, sidesNr, bottomNr, accentColor, gaze) {
         color = this.colors[color];
-        backgroundColor = this.bgColors[backgroundColor]
-        accentColor = this.accentColors[accentColor]
+        backgroundColor = this.bgColors[backgroundColor];
+        accentColor = this.accentColors[accentColor];
         return `
             <g style="color:${color}; fill:${accentColor};">
                 <rect fill="${backgroundColor}" x="0" y="0" width="160" height="160"></rect>
@@ -35,18 +35,28 @@ class Iqons {
             </g>`
     }
 
-    static _$svg(content) {
+    static _$svg(content, gaze) {
         return `
             <svg viewBox="0 0 160 160" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink" >
                 <clipPath id="hexagon-clip">
                     <path d="M88.1247411,2.6381084 L142.907644,34.2670322 C147.858061,37.125157 150.907644,42.4071893 150.907644,48.1234387 L150.907644,111.381286 C150.907644,117.097536 147.858061,122.379568 142.907644,125.237693 L88.1247411,156.866617 C83.1743239,159.724741 77.0751584,159.724741 72.1247411,156.866617 L17.3418381,125.237693 C12.3914209,122.379568 9.34183808,117.097536 9.34183808,111.381286 L9.34183808,48.1234387 C9.34183808,42.4071893 12.3914209,37.125157 17.3418381,34.2670322 L72.1247411,2.6381084 C77.0751584,-0.220016318 83.1743239,-0.220016318 88.1247411,2.6381084 Z" id="Polygon" stroke="#000000" fill="#000000" transform="translate(80.124741, 79.752363) rotate(30.000000) translate(-80.124741, -79.752363) "></path>
                 </clipPath>
-                <g clip-path="url(#hexagon-clip)"${content}</g>
+                    <g clip-path="url(#hexagon-clip)">
+                        <g ${this._$flip(gaze)}>
+                            ${content}
+                        </g>
+                    </g>
             </svg>`
     }
 
     static _$use(part, index) {
         return `<use width="160" height="160" xlink:href="${location.origin}/iqons/dist/iqons.min.svg#${part}-${this._assetIndex(index,part)}"/>`;
+    }
+
+    static _$flip(gaze) {
+        const doFlip = this._assetIndex(gaze, 'gaze') - 1;
+        if (!doFlip) return '';
+        // return 'transform-origin="center center" transform="scale(-1,1)"'
     }
 
     static get colors() {
@@ -103,13 +113,17 @@ class Iqons {
     static get assetCounts() {
         return {
             'face': 24,
-            'side': 18,
+            'side': 20,
             'top': 20,
-            'bottom': 17
+            'bottom': 20,
+            'gaze': 2
         }
     }
 
-    static _assetIndex(index, part) { index = Number(index) % Iqons.assetCounts[part] + 1; return index }
+    static _assetIndex(index, part) {
+        index = (Number(index) % Iqons.assetCounts[part]) + 1;
+        return index
+    }
 
     static _hash(text) {
         return ('' + text
