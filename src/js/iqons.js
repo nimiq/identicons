363dacc6446c6ccf541986188204bf735788513f
sisou <hello@soerenschwert.de>
2018-02-17 14:51:49 -0600

    Add white border to SVG file

--- src/js/iqons.js
+++ src/js/iqons.js
@@ -39,14 +39,24 @@ class Iqons {
     static _$svg(content, gaze) {
         return `
             <svg viewBox="0 0 160 160" width="160" height="160" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink" >
-                <clipPath id="hexagon-clip">
-                    <path d="M88.1247411,2.6381084 L142.907644,34.2670322 C147.858061,37.125157 150.907644,42.4071893 150.907644,48.1234387 L150.907644,111.381286 C150.907644,117.097536 147.858061,122.379568 142.907644,125.237693 L88.1247411,156.866617 C83.1743239,159.724741 77.0751584,159.724741 72.1247411,156.866617 L17.3418381,125.237693 C12.3914209,122.379568 9.34183808,117.097536 9.34183808,111.381286 L9.34183808,48.1234387 C9.34183808,42.4071893 12.3914209,37.125157 17.3418381,34.2670322 L72.1247411,2.6381084 C77.0751584,-0.220016318 83.1743239,-0.220016318 88.1247411,2.6381084 Z" id="Polygon" stroke="#000000" fill="#000000" transform="translate(80.124741, 79.752363) rotate(30.000000) translate(-80.124741, -79.752363) "></path>
-                </clipPath>
-                    <g clip-path="url(#hexagon-clip)">
-                        <g ${this._$flip(gaze)}>
-                            ${content}
-                        </g>
+                <defs>
+                    <clipPath id="hexagon-clip" transform="scale(0.5) translate(0, 16)">
+                        <path d="M251.6 17.34l63.53 110.03c5.72 9.9 5.72 22.1 0 32L251.6 269.4c-5.7 9.9-16.27 16-27.7 16H96.83c-11.43 0-22-6.1-27.7-16L5.6 159.37c-5.7-9.9-5.7-22.1 0-32L69.14 17.34c5.72-9.9 16.28-16 27.7-16H223.9c11.43 0 22 6.1 27.7 16z"/>
+                    </clipPath>
+                    <!--<filter id="gaussian-blur" >
+                      <feGaussianBlur in="SourceAlpha" stdDeviation="5" result="blurOut" />
+                      <feOffset in="blurOut" dx="1" dy="3" result="offsetBlur" />
+                      <feFlood flood-color="#333" flood-opacity="0.71" result="offsetColor"/>
+                      <feComposite in="offsetColor" in2="offsetBlur" operator="in" result="offsetBlur"/>
+                      <feBlend in="SourceGraphic" in2="offsetBlur" mode="normal" />
+                    </filter>-->
+                </defs>
+                <path fill="white" transform="scale(0.5)" d="M251.6 17.34l63.53 110.03c5.72 9.9 5.72 22.1 0 32L251.6 269.4c-5.7 9.9-16.27 16-27.7 16H96.83c-11.43 0-22-6.1-27.7-16L5.6 159.37c-5.7-9.9-5.7-22.1 0-32L69.14 17.34c5.72-9.9 16.28-16 27.7-16H223.9c11.43 0 22 6.1 27.7 16z"/>
+                <g transform="scale(0.9) translate(9, 0)" <!--filter="url(#gaussian-blur)"-->>
+                    <g ${this._$flip(gaze)} clip-path="url(#hexagon-clip)">
+                        ${content}
                     </g>
+                </g>
             </svg>`
     }
 
