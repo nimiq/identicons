f404ef4b542582747d5dbffd5412e3368f7a04e8
sisou <hello@soerenschwert.de>
2018-02-17 13:15:01 -0600

    Minify JS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
-var svgstore = require('gulp-svgstore');
-var svgmin = require('gulp-svgmin');
-var rename = require('gulp-rename');
 var path = require('path');
+var svgmin = require('gulp-svgmin');
+var svgstore = require('gulp-svgstore');
+var cheerio = require('gulp-cheerio');
+var rename = require('gulp-rename');
+var uglify_es = require('uglify-es');
+var composer = require('gulp-uglify/composer');
+var uglify = composer(uglify_es, console);
+var concat = require('gulp-concat');
 
-gulp.task('svgstore', function () {
+gulp.task('prepare-svg', function () {
     return gulp
         .src('src/svg/*/*.svg')
+        // Minify SVG files
         .pipe(svgmin(function (file) {
             var prefix = path.basename(file.relative, path.extname(file.relative));
             return {
@@ -33,9 +39,39 @@ gulp.task('svgstore', function () {
                 ]
             };
         }))
+        // Combine SVG files as symbols
         .pipe(svgstore({ inlineSvg: true }))
+        // Remove unused tags
+        .pipe(cheerio({
+            run: function ($, file) {
+                $('linearGradient').remove();
+                $('radialGradient').remove();
+                $('style').remove();
+                $('path').each(function(i, el) {
+                    var fillAttr = $(el).attr('fill');
+                    if (fillAttr && fillAttr.indexOf('url(#linear-gradient') === 0) {
+                        $(el).remove();
+                    }
+                });
+            },
+            parserOptions: {
+                xmlMode: true
+            }
+        }))
         .pipe(rename('iqons.min.svg'))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['svgstore']);
+gulp.task('prepare-js', function () {
+    return gulp
+        .src(['src/js/iqons-catalog.js', 'src/js/iqons.js'])
+        .pipe(uglify({
+            warnings: true,
+            compress: {},
+            mangle: true
+        }))
+        .pipe(concat('iqons.min.js'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['prepare-svg', 'prepare-js']);
