857c65ba62af2684284d8f059016daf8a78aa000
sisou <hello@soerenschwert.de>
2019-02-08 11:38:25 -0600

    Remove inline option, remove whitespace, remove gaze

--- src/js/iqons.js
+++ src/js/iqons.js
@@ -6,9 +6,9 @@ export default class Iqons {
 
     /* Public API */
 
-    static async svg(text, inline = false) {
+    static async svg(text) {
         const hash = this._hash(text);
-        return this._svgTemplate(hash[0], hash[2], hash[3] + hash[4], hash[5] + hash[6], hash[7] + hash[8], hash[9] + hash[10], hash[11], hash[12], inline);
+        return this._svgTemplate(hash[0], hash[2], hash[3] + hash[4], hash[5] + hash[6], hash[7] + hash[8], hash[9] + hash[10], hash[11], hash[12]);
     }
 
     static async render(text, $element) {
@@ -16,18 +16,17 @@ export default class Iqons {
     }
 
     static async toDataUrl(text) {
-        const base64string = btoa(await this.svg(text, true));
-        return `data:image/svg+xml;base64,${base64string.replace(/#/g, '%23')}`;
+        const base64string = btoa(await this.svg(text, true)); // .replace(/#/g, '%23')
+        return `data:image/svg+xml;base64,${base64string}`;
     }
 
     static placeholder(color = '#bbb', strokeWidth = 1) {
         return `<svg viewBox="0 0 160 160" width="160" height="160" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink" >
-            <path fill="none" stroke="${color}" stroke-width="${2 * strokeWidth}" transform="translate(0, 8) scale(0.5)" d="M251.6 17.34l63.53 110.03c5.72 9.9 5.72 22.1 0 32L251.6 269.4c-5.7 9.9-16.27 16-27.7 16H96.83c-11.43 0-22-6.1-27.7-16L5.6 159.37c-5.7-9.9-5.7-22.1 0-32L69.14 17.34c5.72-9.9 16.28-16 27.7-16H223.9c11.43 0 22 6.1 27.7 16z"/>
-            <g transform="scale(0.9) translate(9, 8)">
-                <circle cx="80" cy="80" r="40" fill="none" stroke="${color}" stroke-width="${strokeWidth}" opacity=".9"></circle>
-                <g opacity=".1" fill="#010101"><path d="M119.21,80a39.46,39.46,0,0,1-67.13,28.13c10.36,2.33,36,3,49.82-14.28,10.39-12.47,8.31-33.23,4.16-43.26A39.35,39.35,0,0,1,119.21,80Z"/></g>\`
-            </g>
-        </svg>`;
+<path fill="none" stroke="${color}" stroke-width="${2 * strokeWidth}" transform="translate(0, 8) scale(0.5)" d="M251.6 17.34l63.53 110.03c5.72 9.9 5.72 22.1 0 32L251.6 269.4c-5.7 9.9-16.27 16-27.7 16H96.83c-11.43 0-22-6.1-27.7-16L5.6 159.37c-5.7-9.9-5.7-22.1 0-32L69.14 17.34c5.72-9.9 16.28-16 27.7-16H223.9c11.43 0 22 6.1 27.7 16z"/>
+<g transform="scale(0.9) translate(9, 8)">
+<circle cx="80" cy="80" r="40" fill="none" stroke="${color}" stroke-width="${strokeWidth}" opacity=".9"></circle>
+<g opacity=".1" fill="#010101"><path d="M119.21,80a39.46,39.46,0,0,1-67.13,28.13c10.36,2.33,36,3,49.82-14.28,10.39-12.47,8.31-33.23,4.16-43.26A39.35,39.35,0,0,1,119.21,80Z"/></g>
+</g></svg>`;
     }
 
     static renderPlaceholder($element, color, strokeWidth) {
@@ -47,14 +46,12 @@ export default class Iqons {
     }
 
     /* Private API */
-    static async _svgTemplate(color, backgroundColor, faceNr, topNr, sidesNr, bottomNr, accentColor, gaze, inline) {
-        return this._$svg(
-            await this._$iqons(color, backgroundColor, faceNr, topNr, sidesNr, bottomNr, accentColor, inline),
-            gaze
-        );
+
+    static async _svgTemplate(color, backgroundColor, faceNr, topNr, sidesNr, bottomNr, accentColor) {
+        return this._$svg(await this._$iqons(color, backgroundColor, faceNr, topNr, sidesNr, bottomNr, accentColor));
     }
 
-    static async _$iqons(color, backgroundColor, faceNr, topNr, sidesNr, bottomNr, accentColor, inline) {
+    static async _$iqons(color, backgroundColor, faceNr, topNr, sidesNr, bottomNr, accentColor) {
 
         color = parseInt(color);
         backgroundColor = parseInt(backgroundColor);
@@ -70,53 +67,33 @@ export default class Iqons {
         backgroundColor = this.backgroundColors[backgroundColor];
         accentColor = this.colors[accentColor];
         return `<g color="${color}" fill="${accentColor}">
-            <rect fill="${backgroundColor}" x="0" y="0" width="160" height="160"></rect>
-            <circle cx="80" cy="80" r="40" fill="${color}"></circle>
-            <g opacity=".1" fill="#010101"><path d="M119.21,80a39.46,39.46,0,0,1-67.13,28.13c10.36,2.33,36,3,49.82-14.28,10.39-12.47,8.31-33.23,4.16-43.26A39.35,39.35,0,0,1,119.21,80Z"/></g>
-            ${await this._generatePart('top',topNr,inline)}
-            ${await this._generatePart('side',sidesNr,inline)}
-            ${await this._generatePart('face',faceNr,inline)}
-            ${await this._generatePart('bottom',bottomNr,inline)}
-        </g>`;
+<rect fill="${backgroundColor}" x="0" y="0" width="160" height="160"></rect>
+<circle cx="80" cy="80" r="40" fill="${color}"></circle>
+<g opacity=".1" fill="#010101"><path d="M119.21,80a39.46,39.46,0,0,1-67.13,28.13c10.36,2.33,36,3,49.82-14.28,10.39-12.47,8.31-33.23,4.16-43.26A39.35,39.35,0,0,1,119.21,80Z"/></g>
+${await this._generatePart('top', topNr)}
+${await this._generatePart('side', sidesNr)}
+${await this._generatePart('face', faceNr)}
+${await this._generatePart('bottom', bottomNr)}
+</g>`;
     }
 
-    static _$svg(content, gaze) { // eslint-disable-line no-unused-vars
-        /** Add this to <defs> for blur:
-        <filter id="gaussian-blur" >
-          <feGaussianBlur in="SourceAlpha" stdDeviation="5" result="blurOut" />
-          <feOffset in="blurOut" dx="1" dy="3" result="offsetBlur" />
-          <feFlood flood-color="#333" flood-opacity="0.71" result="offsetColor"/>
-          <feComposite in="offsetColor" in2="offsetBlur" operator="in" result="offsetBlur"/>
-          <feBlend in="SourceGraphic" in2="offsetBlur" mode="normal" />
-        </filter>
-        */
-
-        /** and this to the <g transform... as an attribute:
-        filter="url(#gaussian-blur)"
-        */
-
+    static _$svg(content) { // eslint-disable-line no-unused-vars
         const randomId = this._getRandomId();
         return `<svg viewBox="0 0 160 160" width="160" height="160" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink" >
-            <defs>
-                <clipPath id="hexagon-clip-${ randomId }" transform="scale(0.5) translate(0, 16)">
-                    <path d="M251.6 17.34l63.53 110.03c5.72 9.9 5.72 22.1 0 32L251.6 269.4c-5.7 9.9-16.27 16-27.7 16H96.83c-11.43 0-22-6.1-27.7-16L5.6 159.37c-5.7-9.9-5.7-22.1 0-32L69.14 17.34c5.72-9.9 16.28-16 27.7-16H223.9c11.43 0 22 6.1 27.7 16z"/>
-                </clipPath>
-            </defs>
-            <g clip-path="url(#hexagon-clip-${ randomId })">
-                ${ content }
-            </g>
-        </svg>`;
+<defs><clipPath id="hexagon-clip-${ randomId }" transform="scale(0.5) translate(0, 16)">
+<path d="M251.6 17.34l63.53 110.03c5.72 9.9 5.72 22.1 0 32L251.6 269.4c-5.7 9.9-16.27 16-27.7 16H96.83c-11.43 0-22-6.1-27.7-16L5.6 159.37c-5.7-9.9-5.7-22.1 0-32L69.14 17.34c5.72-9.9 16.28-16 27.7-16H223.9c11.43 0 22 6.1 27.7 16z"/>
+</clipPath></defs>
+<g clip-path="url(#hexagon-clip-${ randomId })">
+${ content }
+</g></svg>`;
     }
 
-    static async _generatePart(part, index, inline = false) {
-        if (!inline) {
-            return `<use width="160" height="160" xlink:href="${self.NIMIQ_IQONS_SVG_PATH || this.svgPath}#${part}_${this._assetIndex(index, part)}"/>`;
-        } else {
-            const assets = await this._getAssets();
-            const selector = '#' + part + '_' + this._assetIndex(index, part);
-            const $part = assets.querySelector(selector);
-            return $part.innerHTML;
-        }
+    static async _generatePart(part, index) {
+        const assets = await this._getAssets();
+        const selector = '#' + part + '_' + this._assetIndex(index, part);
+        const $part = assets.querySelector(selector);
+        if (!$part) return ''; // Required for catalog.html and as a safeguard
+        return $part.innerHTML;
     }
 
     static _loadImage(dataUrl) {
@@ -136,12 +113,6 @@ export default class Iqons {
             })
         );
     }
-
-    /*static _$flip(gaze) {
-        const doFlip = this._assetIndex(gaze, 'gaze') - 1;
-        if (!doFlip) return '';
-        // return 'transform-origin="center center" transform="scale(-1,1)"'
-    }*/
 
     static get colors() {
         return [
@@ -179,7 +150,6 @@ export default class Iqons {
             'side': IqonsCatalog.side.length,
             'top': IqonsCatalog.top.length,
             'bottom': IqonsCatalog.bottom.length,
-            'gaze': 2
         }
     }
 
