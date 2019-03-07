ba273c47c5bc8d6db1b235e3a1701b0ee6867f4b
sisou <hello@soerenschwert.de>
2019-03-07 16:17:43 +0100

    Uppercase RGB codes for consistency

--- src/js/colors.js
+++ src/js/colors.js
@@ -35,7 +35,7 @@ export const colors = [
     '#1A5493', // indigo-600
     '#0582CA', // light-blue-500
     '#5961A8', // purple-600
-    '#21bca5', // teal-500
+    '#21BCA5', // teal-500
     '#FA7268', // pink-300
     '#88B04B', // light-green-600
     '#795548', // brown-400
@@ -48,7 +48,7 @@ export const backgroundColors = [
     '#1F2348', //
     '#0582CA', // light-blue-500
     '#5F4B8B', //
-    '#21bca5', // teal-500
+    '#21BCA5', // teal-500
     '#FA7268', // pink-300
     '#88B04B', // light-green-600
     '#795548', // brown-400
