f67f9b80d8c1cb5fb3acf11a036cdc535cd9258a
sisou <hello@soerenschwert.de>
2020-05-07 13:39:05 +0200

    Remove obsolete cleanup code in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -20,13 +20,8 @@ gulp.task('prepare-svg', function () {
             return {
                 plugins: [
                     {
-                        inlineStyles: {
-                            onlyMatchedOnce: false
-                        }
-                    },
-                    {
                         removeAttrs: {
-                            attrs: ['data.*', 'viewBox']
+                            attrs: ['viewBox']
                         }
                     },
                     {
@@ -37,8 +32,8 @@ gulp.task('prepare-svg', function () {
                             prefix: prefix,
                             minify: true
                         }
-                    }
-                ]
+                    },
+                ],
             };
         }))
         // Combine SVG files as symbols
@@ -46,25 +41,6 @@ gulp.task('prepare-svg', function () {
         // Remove unused tags
         .pipe(cheerio({
             run: function ($, file) {
-                /**
-                 * We currently remove all gradients in the source SVG files, to keep the Iqons in flat style.
-                 * Therefore, we can also remove all clip paths from the combined SVG file, because they are only
-                 * used by gradients. #g92-5 is the last remaining element that uses the clip-path attribute, but
-                 * is empty without the gradients anyway, so can be removed.
-                 */
-                $('linearGradient').remove();
-                $('radialGradient').remove();
-                $('defs').remove();
-                $('clipPath').remove();
-                $('#g92-5').remove();
-
-                $('style').remove();
-                $('path').each(function(i, el) {
-                    var fillAttr = $(el).attr('fill');
-                    if (fillAttr && fillAttr.indexOf('url(#linear-gradient') === 0) {
-                        $(el).remove();
-                    }
-                });
                 $('[fill="#0f0"]').attr('fill', 'currentColor');
             },
             parserOptions: {
